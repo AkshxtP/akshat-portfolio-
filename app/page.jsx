0d5d694a4ba06feef28f4e1b4bbc84ae6f2bdb95
AkshxtP <akshatp7156@gmail.com>
2025-04-16 01:10:23 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,10 @@
-
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-4">Akshat Pandey</h1>
+      <p className="text-lg text-center max-w-xl">
+        First-year Computer Science student and an aspiring software developer, currently exploring different domains in tech to discover where my true passion lies.
+      </p>
+    </main>
+  );
+}
