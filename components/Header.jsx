7823014f991e665a2b3a508dd82222bc066cdce4
AkshxtP <akshatp7156@gmail.com>
2025-04-16 01:11:47 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,1 +1,8 @@
-
+export default function Header() {
+  return (
+    <header className="w-full text-center py-4 bg-gray-100">
+      <h2 className="text-xl font-semibold">Akshat Pandey</h2>
+      <p className="text-sm">CSE Undergrad / Web Developer</p>
+    </header>
+  );
+}
